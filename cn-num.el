86d61a5eb579e537e6279c1ef2eff7b2ce204697
zbiao <zhcosin@163.com>
2020-05-03 12:45:38 +0800

    fix a bug about chinese zero.

--- cn-num.el
+++ cn-num.el
@@ -3,11 +3,6 @@
 (defvar cn-num--digit-alist '(("零" . 0) ("一" . 1) ("二" . 2) ("三" . 3) ("四" . 4) ("五" . 5) ("六" . 6) ("七" . 7) ("八" . 8) ("九" . 9)))
 (defvar cn-num--weight-alist '(("" . 0) ("十" . 1) ("百" . 2) ("千" . 3)))
 (defvar cn-num--high-weight-alist '(("" . 0) ("万" . 4) ("亿" . 8) ("兆" . 12) ("京" . 16) ("垓" . 20) ("秭" . 24) ("穰" . 28)))
-(defvar cn-num--debug-trace t) ;; debug switch.
-
-(defun cn-num--trace-log(format-str &rest args)
-  (when cn-num--debug-trace
-    (message format-str args)))
 
 (defun cn-num--get-arabic-digit (chinese-digit)
   (assoc-default chinese-digit cn-num--digit-alist))
@@ -34,7 +29,7 @@
   (cn-num--get-weight-name-by-value weight-value cn-num--high-weight-alist))
 
 (defun cn-num--convert-chinese-to-arabic-iter (rest-str result weight-alist weight-index over-weight-convertor)
-  ;;(cn-num--trace-log "call cn-num--convert-arabic-to-chinese-iter with param: rest-str=%s, result=%d, weight-alist=%s, weight-index=%d, over-weight-convertor=%s."
+  ;;(message "call cn-num--convert-arabic-to-chinese-iter with param: rest-str=%s, result=%d, weight-alist=%s, weight-index=%d, over-weight-convertor=%s."
 	   ;;rest-str result weight-alist weight-index over-weight-convertor)
   (setq rest-str (replace-regexp-in-string "零" "" rest-str))
   (if (= (length rest-str) 0)
@@ -43,11 +38,11 @@
            (weight-at-index (nth weight-index weight-alist))
            (weight-separator (car weight-at-index))
            (weight-value (cdr weight-at-index)))
-      (message "weight-at-index=%s." weight-at-index)
+      ;;(message "weight-at-index=%s." weight-at-index)
       (if (= 0 weight-value)
 	  (+ result (* (funcall over-weight-convertor rest-str) (expt 10 weight-value)))
 	(setq rest-list (split-string rest-str weight-separator))
-	(message "string list after split by %s: %s" weight-separator rest-list)
+	;;(message "string list after split by %s: %s" weight-separator rest-list)
         (if (not (= 2 (length rest-list)))
           (cn-num--convert-chinese-to-arabic-iter
 	    rest-str
@@ -77,7 +72,7 @@
       (let* ((weight-at-index (nth weight-index weight-alist))
 	     (quotient (/ arabic-number (expt 10 (cdr weight-at-index))))
 	     (remainder (% arabic-number (expt 10 (cdr weight-at-index)))))
-        (message "arabic-number=%d, weight-at-index=%s, quotient=%d, remainder=%d." arabic-number weight-at-index quotient remainder)
+        ;;(message "arabic-number=%d, weight-at-index=%s, quotient=%d, remainder=%d." arabic-number weight-at-index quotient remainder)
         (if (= 0 quotient)
           (cn-num--convert-arabic-to-chinese-iter
             arabic-number
@@ -86,7 +81,7 @@
             (- weight-index 1)
             over-weight-convertor
             zero-prefix)
-          (let ((newresult (concat result (funcall over-weight-convertor quotient (< 0 (length result))) (car weight-at-index))))
+          (let ((newresult (concat result (funcall over-weight-convertor quotient) (car weight-at-index))))
             (cn-num--convert-arabic-to-chinese-iter
               remainder
 	      newresult
@@ -95,46 +90,20 @@
               over-weight-convertor
               (< 0 (length newresult))))))))
 
-(defun cn-num--compress-chinese-zero(chinese-number)
+(defun cn-num--compress-chinese-zero(chinese-number enable-zero-prefix)
   (message "compress-chinese-zero: %s." chinese-number)
-  (let ((after-compress
-    (replace-regexp-in-string "零+" "零" (replace-regexp-in-string "^零+\\([^零]\\)\\(.*\\)$" "\\1\\2" (replace-regexp-in-string "\\(.*\\)\\([^零]\\)零+$" "\\1\\2" chinese-number)))))
-    (message "after-compress: %s." after-compress)
+  (let ((after-compress chinese-number))
+    (setq after-compress (replace-regexp-in-string "\\(.*\\)\\([^零]\\)零+$" "\\1\\2" chinese-number))
+    (unless enable-zero-prefix
+      (setq after-compress (replace-regexp-in-string "^零+\\([^零]\\)\\(.*\\)$" "\\1\\2" after-compress)))
+    (setq after-compress (replace-regexp-in-string "零+" "零" after-compress))
+    (message "after compress: %s." after-compress)
     after-compress))
           	 
 (defun cn-num--convert-arabic-to-chinese-small-than-10000 (chinese-number &optional zero-prefix)
-  (cn-num--compress-chinese-zero (cn-num--convert-arabic-to-chinese-iter chinese-number "" cn-num--weight-alist (- (length cn-num--weight-alist) 1) (lambda (x y) (cn-num--get-chinese-digit x)) zero-prefix)))
+  (cn-num--compress-chinese-zero (cn-num--convert-arabic-to-chinese-iter chinese-number "" cn-num--weight-alist (- (length cn-num--weight-alist) 1) 'cn-num--get-chinese-digit zero-prefix) zero-prefix))
 
 (defun cn-num--convert-arabic-to-chinese (chinese-number)
-  (cn-num--compress-chinese-zero (cn-num--convert-arabic-to-chinese-iter chinese-number "" cn-num--high-weight-alist (- (length cn-num--high-weight-alist) 1) (lambda (x y) (cn-num--convert-arabic-to-chinese-small-than-10000 x y)) nil)))
-;;(cn-num--convert-chinese-to-arabic-small-than-10000 "七百八十")
-;;(cn-num--convert-chinese-to-arabic-small-than-10000 "一千零五")
-;;(cn-num--convert-chinese-to-arabic-small-than-10000 "三千零二十")
-;;(cn-num--convert-chinese-to-arabic-small-than-10000 "一")
-;;(cn-num--convert-chinese-to-arabic-small-than-10000 "九千九百九十九")
+  (cn-num--compress-chinese-zero (cn-num--convert-arabic-to-chinese-iter chinese-number "" cn-num--high-weight-alist (- (length cn-num--high-weight-alist) 1) (lambda (x) (cn-num--convert-arabic-to-chinese-small-than-10000 x t)) nil) nil))
 
-;;(cn-num--convert-chinese-to-arbic "一千零五亿零三百")
-;;(cn-num--convert-chinese-to-arbic "二百亿零七百八十万零三十二")
-  
-;;(cn-num--convert-arabic-to-chinese-small-than-10000 1010)
-;;(cn-num--convert-arabic-to-chinese 100599090)
-;;(cn-num--convert-arabic-to-chinese 1345300599090)
-;;(cn-num--convert-arabic-to-chinese 21300000000090)
-;;(cn-num--convert-arabic-to-chinese 0)
-
-;;(cn-num--get-weight-name 3)
-;;(cn-num--get-high-weight-value "京")
-;;(cn-num--get-arabic-digit "八")
-;;(cn-num--get-chinese-digit 8)
-
-;;(chinese-number--convert-arabic-to-chinese 20007800032)
-;;"20007800032 = 二百亿零七百八十万零三十二"
-;;(split-string "二百亿零七百八十万零三十二" "") 
-
-;;(replace-regexp-in-string "零" "" "零零零三零七零")
-
-;;(reverse cn-num--high-weight-alist)
-
-;;(string-match-p "零$" "零二零")
-
-(cn-num--trace-log "%s:%d" "abc" 345)
+(provide 'cn-num)
